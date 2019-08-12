4666821843c65a197021f6519df408f206445cde
fuad <fuadqerja>
2019-08-12 23:45:40 +0700

    change API key

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 export default {
   PORT: "3001",
   NODE_ENV: "development",
-  // API_KEY: "c9f1a3a30dc8432cb4223fe7ecc71a1b",
-  API_KEY: "90a31c5ea4b045e9b8137e16f2f995ae",
+  API_KEY: "e57ccf6123f546c1b657c9064e8a4034",
   BASE_URL: "https://newsapi.org/v2",
   DEVICE_ID: "3242351"
 };
